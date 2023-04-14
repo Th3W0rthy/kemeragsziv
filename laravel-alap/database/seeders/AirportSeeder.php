fcf7e906924f885603ecd95b99b013031f7eeb97
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-14 15:21:47 +0200

    AirportSeeder update

--- laravel-alap/database/seeders/AirportSeeder.php
+++ laravel-alap/database/seeders/AirportSeeder.php
@@ -19,6 +19,7 @@ class AirportSeeder extends Seeder
             [
                 'name' => "Ferihegy International Airport",
                 'iata' => "BUD",
+                'time_zone' => "UTC+01:00",
                 'phone' => "+36123456789",
                 'email' => "repter@bud.hu",
                 'continent' => "Europe",
@@ -28,6 +29,7 @@ class AirportSeeder extends Seeder
             [
                 'name' => "Heathrow",
                 'iata' => "LHR",
+                'time_zone' => "UTC+00:00",
                 'phone' => "+44123342345",
                 'email' => "airport@lhr.uk",
                 'continent' => "Europe",
@@ -37,6 +39,7 @@ class AirportSeeder extends Seeder
             [
                 'name' => "Transylvania Airport",
                 'iata' => "TGM",
+                'time_zone' => "UTC+02:00",
                 'phone' => "+40243565246",
                 'email' => "aeroport@tgm.ro",
                 'continent' => "Europa",
@@ -46,6 +49,7 @@ class AirportSeeder extends Seeder
             [
                 'name' => "Frankfurt Airport",
                 'iata' => "FRA",
+                'time_zone' => "UTC+01:00",
                 'phone' => "+49123553201",
                 'email' => "flughafen@.de",
                 'continent' => "Europa",
