caf7fcb759ef598739e624d9fcc870282f1147e0
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-14 15:53:52 +0200

    create_tickets_table

---
+++ laravel-alap/database/migrations/2023_04_14_133907_create_tickets_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('flight_id');
+            $table->string('name', 255);
+            $table->string('sex', 6);
+            $table->dateTime('birth');
+            $table->string('email', 255);
+            $table->string('phone', 12);
+            $table->string('billing_name', 255);
+            $table->string('country', 255);
+            $table->integer('postcode');
+            $table->string('settlement', 255);
+            $table->string('address', 255);
+            $table->integer('tax_number');
+            $table->integer('price');
+            $table->integer('seat_id');
+            $table->timestamps('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
