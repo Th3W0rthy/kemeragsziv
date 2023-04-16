82d389104b7aa53dac310c8e7d69a761991459ef
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-16 20:01:16 +0200

    created_classes_aircrafts

---
+++ laravel-alap/database/migrations/2023_04_16_175847_create_classes_aircraft_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes_aircraft', function (Blueprint $table) {
+            //$table->id();
+            $table->integer('class_id');
+            $table->integer('aircraft_id');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes_aircraft');
+    }
+};
