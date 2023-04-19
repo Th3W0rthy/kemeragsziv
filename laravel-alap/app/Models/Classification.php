a39c9cf4b1d5063c8e3cab250c8138dc74c85a40
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-19 18:51:54 +0200

    classification model

--- laravel-alap/app/Models/Classification.php
+++ laravel-alap/app/Models/Classification.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Classification extends Model
 {
-    use HasFactory;
+    protected $table = "classes";
+    protected $fillable = ['class'];
+    public $timestamps = false;
+
+    public function seats()
+    {
+        return $this->belongsTo(Seat::class);
+    }
+
+    public function classes_aircrafts()
+    {
+        return $this->belongsToMany(Class_aircraft::class);
+    }
 }
