b493b548e2d1b8fcb98a3a141357f98bc849f5d5
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-19 18:51:32 +0200

    class_aircraft model

--- laravel-alap/app/Models/Class_aircraft.php
+++ laravel-alap/app/Models/Class_aircraft.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Class_aircraft extends Model
 {
-    use HasFactory;
+    protected $table = "classes_aircrafts";
+    protected $fillable = ['class_id','aircraft_id',];
+    public $timestamps = false;
+
+    public function aircrafts()
+    {
+        return $this->belongsToMany(Aircraft::class);
+    }
+
+    public function classes()
+    {
+        return $this->belongsToMany(Classification::class);
+    }
 }
