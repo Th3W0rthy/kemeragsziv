d28bb9dd212e6c626fd644823ffc43bffc2cb855
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-19 18:26:21 +0200

    airport model

--- laravel-alap/app/Models/Airport.php
+++ laravel-alap/app/Models/Airport.php
@@ -7,16 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Airport extends Model
 {
     protected $table = "airports";
+    protected $fillable = ['name','iata','time_zone','phone','email','website','continent','country','settlement'];
     public $timestamps = false;
 
     public function luggages_logs()
     {
-        return $this->belongsTo();
+        return $this->belongsTo(Luggage_log::class);
     }
 
     public function flights()
     {
-        return $this->belongsTo();
-        return $this->belongsTo();
+        return $this->belongsTo(Flight::class);
+        return $this->belongsTo(Flight::class);
     }
 }
