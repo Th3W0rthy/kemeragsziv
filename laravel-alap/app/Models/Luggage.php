ab7a42e755da1108c59c3e913ef8731fc214f733
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-21 14:31:11 +0200

    luggage model relationships

--- laravel-alap/app/Models/Luggage.php
+++ laravel-alap/app/Models/Luggage.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Luggage extends Model
 {
-    use HasFactory;
+    protected $table = "luggages";
+    protected $fillable = ['ticket_id','tag','size','price'];
+    public $timestamps = false;
+
+    public function Luggages_logs()
+    {
+        return $this->hasMany(Luggage_log::class);
+    }
+
+    public function ticket()
+    {
+        return $this->belongsTo(Luggage::class);
+    }
 }
