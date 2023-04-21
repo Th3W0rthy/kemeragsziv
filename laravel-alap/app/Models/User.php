db624554b5909e919e51ad62e17878f8da637d1f
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-21 13:58:02 +0200

    user model relationships

--- laravel-alap/app/Models/User.php
+++ laravel-alap/app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $table = "users";
+    public $timestamps = true;
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
