816da5c068818df79c00545c725509f8c2c3c6a3
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-21 14:45:26 +0200

    seat model relationships

--- laravel-alap/app/Models/Seat.php
+++ laravel-alap/app/Models/Seat.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Seat extends Model
 {
-    use HasFactory;
+    protected $table = "seats";
+    protected $fillable = ['aircraft_id','class_id','seat_row','seat_col'];
+    public $timestamps = false;
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function class()
+    {
+        return $this->belongsTo(Classification::class);
+    }
+
+    public function aircraft()
+    {
+        return $this->belongsTo(Aircraft::class);
+    }
 }
