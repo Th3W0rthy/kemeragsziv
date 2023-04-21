9caec68d66ef1627add87cf61c728c2b9f603e52
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-21 18:32:57 +0200

    update database seeder

--- laravel-alap/database/seeders/DatabaseSeeder.php
+++ laravel-alap/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            AircraftSeeder::class,
+            AirlineSeeder::class,
+            AirportSeeder::class,
+            FlightSeeder::class,
+            TicketSeeder::class,
+            UserSeeder::class,
+            LuggageSeeder::class,
+            Luggage_logSeeder::class,
+            SeatSeeder::class,
+            ClassSeeder::class,
+            Class_aircraftSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
