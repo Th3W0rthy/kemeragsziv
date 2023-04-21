9b0946109a07429c0af6ffc4785fd08efead9b6c
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-21 14:23:07 +0200

    ticket model relationships

--- laravel-alap/app/Models/Ticket.php
+++ laravel-alap/app/Models/Ticket.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    use HasFactory;
+    protected $table = "tickets";
+    protected $fillable = ['user_id','flight_id','name', 'sex', 'birth', 'email', 'phone', 'billing_name','country','postcode','settlement','address','tax_number','price','seat_id'];
+    public $timestamps = true;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function luggages()
+    {
+        return $this->hasMany(Luggage::class);
+    }
+
+    public function flight()
+    {
+        return $this->belongsTo(Flight::class);
+    }
+
+    public function seat()
+    {
+        return $this->belongsTo(Seat::class);
+    }
 }
