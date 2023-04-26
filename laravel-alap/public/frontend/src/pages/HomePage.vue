f07b1b63a76f1e1e7ac14b6f84c2abe9f37a4d72
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-04-26 19:05:35 +0200

    homepage row button

--- laravel-alap/public/frontend/src/pages/HomePage.vue
+++ laravel-alap/public/frontend/src/pages/HomePage.vue
@@ -6,10 +6,15 @@ const FlightStore = useFlight();
 
 const emptyString = "";
 
+let show = false;
+
 async function showFlight(id){
-  
+  if (show == false)
+  {
+    show = true;
+  }
   const flight = await FlightStore.getFlight(id);
-  alert(`${flight.number}, ${show}`);
+  alert(`${flight.number}`);
 }
 
 onMounted(FlightStore.getFlights);
@@ -30,7 +35,7 @@ onMounted(FlightStore.getFlights);
     </thead>
     </table>
     <div id="flights">
-      <table class="table table-striped table-dark" v-for="flight in FlightStore.flights" :key="flight.id" @click="showFlight(flight.id)">
+      <table class="table table-striped table-dark" v-for="flight in FlightStore.flights" :key="flight.id">
         <tbody>
           <tr>
             <td>{{ flight.departure }}</td>
@@ -39,9 +44,13 @@ onMounted(FlightStore.getFlights);
             <td>{{ flight.arrival_airport }}</td>
             <td>{{ flight.airline }}</td>
             <td>{{ flight.number }}</td>
-            <td></td>
+            <td>
+              <button class="btn btn-primary" @click="showFlight(flight.id)">
+                More
+              </button>
+            </td>
           </tr>
-          <tr v-show="show">
+          <tr v-show="asd">
             <td colspan="6">
               <div class="row">
                 <div class="col">
@@ -100,30 +109,5 @@ onMounted(FlightStore.getFlights);
         </tbody>
       </table>
     </div>
-
-    <!--
-    <table class="table table-striped table-dark">
-      <thead>
-        <tr>
-          <th>Departure</th>
-          <th>Arrival</th>
-          <th>From</th>
-          <th>To</th>
-          <th>Airline</th>
-          <th>Flight number</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="flight in FlightStore.flights" :key="flight.id" @click="onClick(flight.id)">
-          <td>{{ flight.departure }}</td>
-          <td>{{ flight.arrival }}</td>
-          <td>{{ flight.departure_airport }}</td>
-          <td>{{ flight.arrival_airport }}</td>
-          <td>{{ flight.airline }}</td>
-          <td>{{ flight.number }}</td>
-        </tr>
-      </tbody>
-    </table>
-    -->
   </main>
 </template>
