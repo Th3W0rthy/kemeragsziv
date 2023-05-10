a562652fb38eed4f409c38b559c1ea7cab5225ed
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2023-05-10 17:28:32 +0200

    Qr code

--- backend/public/frontend/src/pages/GroundHandler.vue
+++ backend/public/frontend/src/pages/GroundHandler.vue
@@ -1,9 +1,53 @@
-<script setup>
-
-</script>
-
 <template>
   <main class="container">
    gh
+   <qrcode-stream @init="onInit"  ></qrcode-stream>
   </main>
 </template>
+
+<script>
+  import { QrcodeStream } from 'vue3-qrcode-reader';
+  export default {
+    data() {
+      return {
+        error: '',
+      }
+    },
+    components: {
+      QrcodeStream
+    },
+    methods: {
+      async onInit( promise ) {
+        // show loading indicator
+
+        try {
+          const { capabilities } =  await promise
+
+          // successfully initialized
+          } catch (error) {
+          if (error.name === 'NotAllowedError') {
+            $this.error = "user denied camera access permisson";
+            alert($this.error);
+          } else if (error.name === 'NotFoundError') {
+            $this.error = "no suitable camera device installed";
+            alert($this.error);
+          } else if (error.name === 'NotSupportedError') {
+            $this.error = "page is not served over HTTPS (or localhost)";
+            alert($this.error);
+          } else if (error.name === 'NotReadableError') {
+            $this.error = "maybe camera is already in use";
+            alert($this.error);
+          } else if (error.name === 'OverconstrainedError') {
+            $this.error = "did you requested the front camera although there is none?";
+            alert($this.error);
+          } else if (error.name === 'StreamApiNotSupportedError') {
+            $this.error = "browser seems to be lacking features";
+            alert($this.error);
+          }
+        } finally {
+          // hide loading indicator
+        }
+      }
+    }
+  }
+</script>
