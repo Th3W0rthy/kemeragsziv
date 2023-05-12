b7dedc91fb0ba2ec4b99fca37aca2e5ffcfe0b0d
SagiDaniel <sagidaniel24@gmail.com>
2023-05-12 16:44:05 +0200

    Test Registration input and Login input

--- Teszt/KemeragszivTest/KemeragszivTest/UnitTest1.cs
+++ Teszt/KemeragszivTest/KemeragszivTest/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace KemeragszivTest
         public void SetUp()
         {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://localhost:5173"); 
+            driver.Navigate().GoToUrl("http://localhost:5173");
         }
 
         [TearDown]
@@ -49,6 +49,24 @@ namespace KemeragszivTest
             Assert.IsTrue(loginLink.Displayed);
             Assert.IsTrue(registerLink.Displayed);
         }
+    }
+    public class LoginTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5173");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
         [Test]
         public void LoginFalseEmailTest() {
             driver.FindElement(By.LinkText("Login")).Click();
@@ -59,6 +77,32 @@ namespace KemeragszivTest
             IWebElement ErrorMessage = driver.FindElement(By.Id("EmailErrormessage"));
             Assert.IsTrue(ErrorMessage.Displayed);
             Assert.AreEqual("email must be a valid email", ErrorMessage.Text);
+        }
+        [Test]
+        public void LoginNullEmailTest()
+        {
+            driver.FindElement(By.LinkText("Login")).Click();
+            IWebElement emailInput = driver.FindElement(By.Id("email"));
+            emailInput.SendKeys("asd");
+            emailInput.SendKeys(Keys.Control + "a");
+            emailInput.SendKeys(Keys.Delete);
+            driver.FindElement(By.Id("password")).SendKeys("ASd");
+            IWebElement ErrorMessage = driver.FindElement(By.Id("EmailErrormessage"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            Assert.AreEqual("The field is mandatory", ErrorMessage.Text);
+        }
+        [Test]
+        public void LoginNullPasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Login")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("asd");
+            passordInput.SendKeys(Keys.Control + "a");
+            passordInput.SendKeys(Keys.Delete);
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = driver.FindElement(By.Id("PasswordErrormessage"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            Assert.AreEqual("The field is mandatory", ErrorMessage.Text);
         }
         [Test]
         public void LoginTrueEmailTest()
@@ -76,11 +120,29 @@ namespace KemeragszivTest
             }
             catch (NoSuchElementException)
             {
-                
+
             }
             Assert.IsNull(ErrorMessage);
         }
-        [Test]
+    }
+    public class RegistrationTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5173");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    [Test]
         public void RegistrationTrueEmailTest()
         {
             driver.FindElement(By.LinkText("Register")).Click();
@@ -110,6 +172,112 @@ namespace KemeragszivTest
             IWebElement ErrorMessage = driver.FindElement(By.Id("EmailErrormessage"));
             Assert.IsTrue(ErrorMessage.Displayed);
             Assert.AreEqual("email must be a valid email", ErrorMessage.Text);
+        }
+        [Test]
+        public void RegistrationNullPasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("asd");
+            passordInput.SendKeys(Keys.Control + "a");
+            passordInput.SendKeys(Keys.Delete);
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = driver.FindElement(By.Id("PasswordErrormessage"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            Assert.AreEqual("The field is mandatory", ErrorMessage.Text);
+        }
+        [Test]
+        public void RegistrationBetweenZeroEightPasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("asdasd");
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = driver.FindElement(By.Id("PasswordErrormessage"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            Assert.AreEqual("Password must contain at least 8 characters, one uppercase, one number and one special case character.", ErrorMessage.Text);
+        }
+        [Test]
+        public void RegistrationNullUpeprcasePasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("asdasdasd");
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = driver.FindElement(By.Id("PasswordErrormessage"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            Assert.AreEqual("Password must contain at least 8 characters, one uppercase, one number and one special case character.", ErrorMessage.Text);
+        }
+        [Test]
+        public void RegistrationNullNumberPasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("Asdasdasd");
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = driver.FindElement(By.Id("PasswordErrormessage"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            Assert.AreEqual("Password must contain at least 8 characters, one uppercase, one number and one special case character.", ErrorMessage.Text);
+        }
+        [Test]
+        public void RegistrationNullSpecialCharacterPasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("Asdasdasd8");
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = driver.FindElement(By.Id("PasswordErrormessage"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            Assert.AreEqual("Password must contain at least 8 characters, one uppercase, one number and one special case character.", ErrorMessage.Text);
+        }
+        [Test]
+        public void RegistrationTreuPasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("Asdasdasd.8");
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = null;
+            try
+            {
+                ErrorMessage = driver.FindElement(By.Id("PasswordErrormessage"));
+            }
+            catch (NoSuchElementException)
+            {
+
+            }
+            Assert.IsNull(ErrorMessage);
+        }
+        [Test]
+        public void RegistrationTrueAgainPasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("Asdasdasd.8");
+            driver.FindElement(By.Id("password_confirmation")).SendKeys("Asdasdasd.8");
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = null;
+            try
+            {
+                ErrorMessage = driver.FindElement(By.Id("AgainPasswordErrormessage"));
+            }
+            catch (NoSuchElementException)
+            {
+
+            }
+            Assert.IsNull(ErrorMessage);
+        }
+        [Test]
+        public void RegistrationFalseAgainPasswordlTest()
+        {
+            driver.FindElement(By.LinkText("Register")).Click();
+            IWebElement passordInput = driver.FindElement(By.Id("password"));
+            passordInput.SendKeys("Asdasdasd.8");
+            driver.FindElement(By.Id("password_confirmation")).SendKeys("Asdasdasd.7");
+            driver.FindElement(By.Id("email")).SendKeys("ASd");
+            IWebElement ErrorMessage = driver.FindElement(By.Id("AgainPasswordErrormessage"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            Assert.AreEqual("Passwords don't match.", ErrorMessage.Text);
         }
     }
 }
